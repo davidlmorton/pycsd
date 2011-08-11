8d349b60abcce5b12b0ee6c5cd4cd8e1df7c495d
David Morton <davidlmorton@gmail.com>
2011-08-11 00:57:25 -0500

    "fixed" problem with step_icsd.py by using numerical integration instead of using the closed form solution.

--- step_icsd.py
+++ step_icsd.py
@@ -1,5 +1,6 @@
 import itertools
 import numpy
+from scipy import integrate
 
 from pycsd import utils
 
@@ -25,15 +26,35 @@ def step_icsd_forward_solution_matrix(depths, diameter, sigma_above):
         ub = z[i] + (h/2.0)
         lb = z[i] - (h/2.0)
         z_j = z[j]
-        this_result = (1/4.0)*(first_term(z_j=z_j, r_sq=r_sq, z=ub) - 
+        if i == j: 
+            # not sure why, but we must use numerical integration to
+            #  get result accurately for i==j.
+            f = lambda x: integrand(x, r_sq=r_sq, z_j=z_j)
+            f_mirror = lambda x: mirror_integrand(x, r_sq=r_sq, z_j=z_j)
+            tissue_term = integrate.quad(f, lb, ub, epsrel=0.01)[0]
+            mirror_term = integrate.quad(f_mirror, lb, ub, epsrel=0.01)[0]
+            this_result = tissue_term + it*mirror_term
+        else:
+            this_result = (1/4.0)*(first_term(z_j=z_j, r_sq=r_sq, z=ub) - 
                                first_term(z_j=z_j, r_sq=r_sq, z=lb) + 
                                second_term(it=it, z_j=z_j, r_sq=r_sq, z=ub) - 
                                second_term(it=it, z_j=z_j, r_sq=r_sq, z=lb)) 
         result[j,i] = this_result
     return result
 
+step_icsd_forward_solution_matrix.__doc__ = \
+        utils.BASIC_FORWARD_SOLUTION_DOCSTRING %\
+        'the CSD is stepwise constant'
+
+def integrand(z, r_sq=None, z_j=None):
+    return (1/2.0)*(numpy.sqrt(r_sq + (z_j-z)**2) - numpy.abs(z_j - z))
+
+def mirror_integrand(z, r_sq=None, z_j=None):
+    return (1/2.0)*(numpy.sqrt(r_sq + (z_j+z)**2) - numpy.abs(z_j + z))
+
 def first_term(z_j=None, r_sq=None, z=None):
     a = (z - z_j)*numpy.sqrt(r_sq + (z_j - z)**2)
+    log_arg = numpy.sqrt(r_sq + (z_j - z)**2) - z_j + z
     b = r_sq*numpy.log(numpy.sqrt(r_sq + (z_j - z)**2) - z_j + z)
     c = z*(z-2*z_j)*numpy.sign(z_j-z)
     return a + b + c
@@ -44,6 +65,3 @@ def second_term(it=None, z_j=None, r_sq=None, z=None):
     c = -z*(z+2*z_j)*numpy.sign(z_j+z)
     return it*(a + b + c)
 
-step_icsd_forward_solution_matrix.__doc__ = \
-        utils.BASIC_FORWARD_SOLUTION_DOCSTRING %\
-        'the CSD is stepwise constant'
--- test/plot_test_figures.py
+++ test/plot_test_figures.py
@@ -11,7 +11,7 @@ smooth_depths = numpy.arange(25, 2525, 25)
 
 # create the sine wave test data
 test_csd = ctd.create_test_csd_data('sine', smooth_depths)
-test_pot = ctd.create_potential_like_data('sine', smooth_depths, diameter=100000,
+test_pot = ctd.create_potential_like_data('sine', smooth_depths, diameter=5000,
         sigma_above='inf')
 
 test_pot_100 = numpy.array([test_pot[i] for i in range(len(test_pot)) 
@@ -19,21 +19,21 @@ test_pot_100 = numpy.array([test_pot[i] for i in range(len(test_pot))
 test_pot_200 = numpy.array([test_pot[i] for i in range(len(test_pot)) 
         if smooth_depths[i] in depths_200])
 
-step_csd_100 = si.step_icsd(test_pot_100, depths_100, diameter=100000, 
+step_csd_100 = si.step_icsd(test_pot_100, depths_100, diameter=5000, 
     sigma_above='inf')
-step_csd_200 = si.step_icsd(test_pot_200, depths_200, diameter=100000, 
-    sigma_above='inf')
-
-delta_csd_100 = di.delta_icsd(test_pot_100, depths_100, diameter=100000,
-    sigma_above='inf')
-delta_csd_200 = di.delta_icsd(test_pot_200, depths_200, diameter=100000,
+step_csd_200 = si.step_icsd(test_pot_200, depths_200, diameter=5000, 
     sigma_above='inf')
 
-pylab.plot(test_csd, -smooth_depths)
-pylab.plot(step_csd_100, -depths_100)
-pylab.plot(step_csd_200, -depths_200)
-pylab.plot(delta_csd_100, -depths_100, linestyle=':', marker='o')
-pylab.plot(delta_csd_200, -depths_200, linestyle=':', marker='o')
+delta_csd_100 = di.delta_icsd(test_pot_100, depths_100, diameter=5000,
+    sigma_above='inf')
+delta_csd_200 = di.delta_icsd(test_pot_200, depths_200, diameter=5000,
+    sigma_above='inf')
+
+pylab.plot(smooth_depths, test_csd)
+pylab.plot(depths_100, step_csd_100, drawstyle='steps-mid')
+pylab.plot(depths_200, step_csd_200, drawstyle='steps-mid')
+pylab.plot(depths_100, delta_csd_100, linestyle=':', marker='o')
+pylab.plot(depths_200, delta_csd_200, linestyle=':', marker='o')
 
 pylab.show()
 
