3f0444523baa1d2461bd0b751867b389deee54f3
davidmorton <davidmorton@sirken.(none)>
2011-08-11 14:49:21 -0500

    Fixed the exact solution form of step_icsd evaluation.

--- step_icsd.py
+++ step_icsd.py
@@ -25,20 +25,12 @@ def step_icsd_forward_solution_matrix(depths, diameter, sigma_above):
     for i, j in itertools.product(range(num_depths), range(num_depths)):
         ub = z[i] + (h/2.0)
         lb = z[i] - (h/2.0)
-        z_j = z[j]
-        if i == j: 
-            # not sure why, but we must use numerical integration to
-            #  get result accurately for i==j.
-            f = lambda x: integrand(x, r_sq=r_sq, z_j=z_j)
-            f_mirror = lambda x: mirror_integrand(x, r_sq=r_sq, z_j=z_j)
-            tissue_term = integrate.quad(f, lb, ub, epsrel=0.01)[0]
-            mirror_term = integrate.quad(f_mirror, lb, ub, epsrel=0.01)[0]
-            this_result = tissue_term + it*mirror_term
-        else:
-            this_result = (1/4.0)*(first_term(z_j=z_j, r_sq=r_sq, z=ub) - 
-                               first_term(z_j=z_j, r_sq=r_sq, z=lb) + 
-                               second_term(it=it, z_j=z_j, r_sq=r_sq, z=ub) - 
-                               second_term(it=it, z_j=z_j, r_sq=r_sq, z=lb)) 
+        this_result = (1/4.0)*(
+                source_term_one(z[j], r_sq, z=ub) -
+                source_term_one(z[j], r_sq, z=lb) -
+                source_term_two(z[j], ub=ub, lb=lb) +
+                image_term(it=it, j=z[j], r_sq=r_sq, z=ub) - 
+                image_term(it=it, j=z[j], r_sq=r_sq, z=lb)) 
         result[j,i] = this_result
     return result
 
@@ -46,22 +38,30 @@ step_icsd_forward_solution_matrix.__doc__ = \
         utils.BASIC_FORWARD_SOLUTION_DOCSTRING %\
         'the CSD is stepwise constant'
 
-def integrand(z, r_sq=None, z_j=None):
-    return (1/2.0)*(numpy.sqrt(r_sq + (z_j-z)**2) - numpy.abs(z_j - z))
+def integrand(z, r_sq=None, j=None):
+    return (1/2.0)*(numpy.sqrt(r_sq + (j-z)**2) - numpy.abs(j - z))
 
-def mirror_integrand(z, r_sq=None, z_j=None):
-    return (1/2.0)*(numpy.sqrt(r_sq + (z_j+z)**2) - numpy.abs(z_j + z))
+def mirror_integrand(z, r_sq=None, j=None):
+    return (1/2.0)*(numpy.sqrt(r_sq + (j+z)**2) - numpy.abs(j + z))
 
-def first_term(z_j=None, r_sq=None, z=None):
-    a = (z - z_j)*numpy.sqrt(r_sq + (z_j - z)**2)
-    log_arg = numpy.sqrt(r_sq + (z_j - z)**2) - z_j + z
-    b = r_sq*numpy.log(numpy.sqrt(r_sq + (z_j - z)**2) - z_j + z)
-    c = z*(z-2*z_j)*numpy.sign(z_j-z)
-    return a + b + c
+def source_term_one(j=None, r_sq=None, z=None):
+    a = (z - j)*numpy.sqrt(r_sq + (j - z)**2)
+    b = r_sq*numpy.log(numpy.sqrt(r_sq + (j - z)**2) - j + z)
+    return a + b
 
-def second_term(it=None, z_j=None, r_sq=None, z=None):
-    a = (z + z_j)*numpy.sqrt(r_sq + (z_j + z)**2)
-    b = r_sq*numpy.log(numpy.sqrt(r_sq + (z_j + z)**2) + z_j + z)
-    c = -z*(z+2*z_j)*numpy.sign(z_j+z)
+def source_term_two(j, ub=None, lb=None):
+    if numpy.average([ub, lb]) == j: # diagonal term
+        # evaluate diagnal term explicitly, since it crosses zero.
+        h = ub-lb
+        c = h**2/2.0
+    else:
+        c_term = lambda z: -z*(z-2*j)*numpy.sign(j-z)
+        c = c_term(ub) - c_term(lb)
+    return c
+
+def image_term(it=None, j=None, r_sq=None, z=None):
+    a = (z + j)*numpy.sqrt(r_sq + (j + z)**2)
+    b = r_sq*numpy.log(numpy.sqrt(r_sq + (j + z)**2) + j + z)
+    c = -z*(z+2*j)
     return it*(a + b + c)
 
